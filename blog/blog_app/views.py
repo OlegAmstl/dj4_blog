3e64cc94edf6d1a2ad826e432e05f6cbf0999d3f
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2023-05-28 07:26:43 +0700

    Созданны представления для вывода всех опубликованных постов и вывода выбранного поста

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    """
+    Отображение всех постов.
+    :param request:
+    :return:
+    """
+    template = "blog_app/post/list.html"
+    posts = Post.published.all()
+    context = {"posts": posts}
+    return render(request, template, context=context)
+
+
+def post_detail(request, id):
+    """
+    Отображение выбранного поста.
+    :param request:
+    :param id:
+    :return:
+    """
+    template = "blog_app/post/detail.html"
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    context = {"post": post}
+    return render(request, template, context=context)
