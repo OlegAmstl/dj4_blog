3e6ffab3278ae7335afa39ccc2fad0599a13bf6f
Oleg <amstl@mail.ru>
2023-06-08 11:14:45 +0700

    Добавлены шаблонные теги для отображения 5 новых постов и 5 постов у которых больше всего комментариев.

--- blog/blog_app/templatetags/blog_tags.py
+++ blog/blog_app/templatetags/blog_tags.py
@@ -1,8 +1,9 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
-
 register = template.Library()
+
 
 @register.simple_tag
 def total_posts():
@@ -10,3 +11,21 @@ def total_posts():
     Показывает общее количество постов.
     """
     return Post.published.count()
+
+
+@register.inclusion_tag('blog_app/post/latest_posts.html')
+def show_latest_posts(count=5):
+    """
+    Показывает последние 5 постов.
+    """
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    """
+    Отображает 5 постов у которых больше всего комментариев.
+    """
+    return Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
